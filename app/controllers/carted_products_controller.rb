5ef42c4eae45b13784a5d85c9550ff4cb5859264
jeffspatafora <jeffspatafora@gmail.com>
2022-04-12 11:16:21 -0700

    cloned app, got working REST functions

---
+++ app/controllers/carted_products_controller.rb
@@ -0,0 +1,7 @@
+class CartedProductsController < ApplicationController
+  # def create
+  #   carted_product = CartedProduct.new(
+  #     user_id = 
+  #   )
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
   
+  
 end
